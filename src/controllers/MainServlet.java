96829b40f8318dd45df2c88269e63e22022f9940
user050-dounyu <user050.dounyu@techacademy.jp>
2024-05-27 15:59:17 +0900

    メイン画面に戻れるように修正

--- src/controllers/MainServlet.java
+++ src/controllers/MainServlet.java
@@ -29,7 +29,7 @@ public class MainServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index/main.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tool/main.jsp");
         rd.forward(request, response);
     }
 
